5c0049dd919461882fc047a6afee58c2d84ba663
raksha1607 <raksharcmali@gmail.com>
2024-11-24 01:42:24 -0800

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+  document.getElementById("display").value += character;
+}
+
+function calculateResult() {
+  const display = document.getElementById("display");
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
